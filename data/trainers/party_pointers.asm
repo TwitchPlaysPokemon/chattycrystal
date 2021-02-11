e055e26aa30d5f00ffba85a508161b096e3de5de
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-11 13:51:45 -0300

    Clean up formatting

--- data/trainers/party_pointers.asm
+++ data/trainers/party_pointers.asm
@@ -2,39 +2,39 @@
 
 TrainerGroups:
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
-	dba FalknerGroup ;00
-	dba WhitneyGroup ;01
-	dba BugsyGroup ;02
-	dba MortyGroup ;03
-	dba PryceGroup ;04
-	dba JasmineGroup ;05
-	dba ChuckGroup ;06
-	dba ClairGroup ;07
-	dba Rival1Group ;08
-	dba PokemonProfGroup ;09
-	dba WillGroup ;0A
-	dba PKMNTrainerGroup ;0B
-	dba BrunoGroup ;0C
-	dba KarenGroup;0D
-	dba KogaGroup;0E
-	dba ChampionGroup;0F
-	dba BrockGroup ;10
-	dba MistyGroup;11
-	dba LtSurgeGroup;12
-	dba ScientistGroup;13
-	dba ErikaGroup;14
-	dba YoungsterGroup;15
-	dba SchoolboyGroup;16
-	dba BirdKeeperGroup;17
-	dba LassGroup ;18
-	dba JanineGroup;19
-	dba CooltrainerMGroup;1A
-	dba CooltrainerFGroup;1B
-	dba BeautyGroup;1C
-	dba PokemaniacGroup;1D
-	dba GruntMGroup;1E
-	dba GentlemanGroup;1F
-	dba SkierGroup ;20
+	dba FalknerGroup        ;00
+	dba WhitneyGroup
+	dba BugsyGroup
+	dba MortyGroup
+	dba PryceGroup
+	dba JasmineGroup
+	dba ChuckGroup
+	dba ClairGroup
+	dba Rival1Group         ;08
+	dba PokemonProfGroup
+	dba WillGroup
+	dba PKMNTrainerGroup
+	dba BrunoGroup
+	dba KarenGroup
+	dba KogaGroup
+	dba ChampionGroup
+	dba BrockGroup          ;10
+	dba MistyGroup
+	dba LtSurgeGroup
+	dba ScientistGroup
+	dba ErikaGroup
+	dba YoungsterGroup
+	dba SchoolboyGroup
+	dba BirdKeeperGroup
+	dba LassGroup           ;18
+	dba JanineGroup
+	dba CooltrainerMGroup
+	dba CooltrainerFGroup
+	dba BeautyGroup
+	dba PokemaniacGroup
+	dba GruntMGroup
+	dba GentlemanGroup
+	dba SkierGroup          ;20
 	dba TeacherGroup
 	dba SabrinaGroup
 	dba BugCatcherGroup
@@ -42,7 +42,7 @@ TrainerGroups:
 	dba SwimmerMGroup
 	dba SwimmerFGroup
 	dba SailorGroup
-	dba SuperNerdGroup ;28
+	dba SuperNerdGroup      ;28
 	dba Rival2Group
 	dba GuitaristGroup
 	dba HikerGroup
@@ -50,15 +50,15 @@ TrainerGroups:
 	dba BlaineGroup
 	dba BurglarGroup
 	dba FirebreatherGroup
-	dba JugglerGroup;30
-	dba BlackbeltGroup 
+	dba JugglerGroup        ;30
+	dba BlackbeltGroup
 	dba ExecutiveMGroup
 	dba PsychicGroup
 	dba PicnickerGroup
 	dba CamperGroup
 	dba ExecutiveFGroup
 	dba SageGroup
-	dba MediumGroup;38
+	dba MediumGroup         ;38
 	dba BoarderGroup
 	dba PokefanMGroup
 	dba KimonoGirlGroup
@@ -66,23 +66,23 @@ TrainerGroups:
 	dba PokefanFGroup
 	dba RedGroup
 	dba BlueGroup
-	dba OfficerGroup;40
-	dba GruntFGroup;41
-	dba MysticalmanGroup;42
-	dba AzureGroup;43
-	dba FCLarryGroup;44
-	dba RBYGreenGroup;45
-	dba CYRoboredGroup;46
-	dba GSCHGSSChrisGroup;47
-	dba GSCKrisGroup;48
-	dba ACChrisGroup;49
-	dba FCChrisGroup;4A
-	dba RSEORASMayGroup;4B
+	dba OfficerGroup        ;40
+	dba GruntFGroup
+	dba MysticalmanGroup
+	dba AzureGroup
+	dba FCLarryGroup
+	dba RBYGreenGroup
+	dba CYRoboredGroup
+	dba GSCHGSSChrisGroup
+	dba GSCKrisGroup        ;48
+	dba ACChrisGroup
+	dba FCChrisGroup
+	dba RSEORASMayGroup
 	dba RSEORASBlueMayGroup
 	dba RSEORASBrendanGroup
 	dba FRLGLeafGroup
 	dba ColoWesGroup
-	dba XDMichaelGroup
+	dba XDMichaelGroup      ;50
 	dba PTDawnGroup
 	dba PTLucasGroup
 	dba HGSSLyraGroup
@@ -90,7 +90,7 @@ TrainerGroups:
 	dba BWHildaGroup
 	dba B2W2RosaGroup
 	dba B2W2NateGroup
-	dba XYCalemGroup
+	dba XYCalemGroup        ;58
 	dba XYSerenaGroup
 	dba XYSerena2Group
 	dba SMUSUMElioGroup
@@ -98,7 +98,7 @@ TrainerGroups:
 	dba SMUSUMSeleneGroup
 	dba SWSHGloriaGroup
 	dba GlazedGroup
-	dba MaribelGroup
+	dba MaribelGroup        ;60
 	dba NuzlockeGroup
 	dba SiriusGroup
 	dba PCyanGroup
@@ -106,7 +106,7 @@ TrainerGroups:
 	dba RedLarryGroup
 	dba GreenMayGroup
 	dba BrownGroup
-	dba BABAGroup
+	dba BABAGroup           ;68
 	dba URFGroup
 	dba SMUSUMElio2Group
 	dba RSEORASBrendanLeaderGroup

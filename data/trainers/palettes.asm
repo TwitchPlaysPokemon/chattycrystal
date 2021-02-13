1577f4bbb65483226f098b7deee9d880be45d86d
chaos <dd4791.mariokarter@hotmail.co.uk>
2021-02-13 16:34:38 +0000

    fix error in huni's party

--- data/trainers/palettes.asm
+++ data/trainers/palettes.asm
@@ -5,66 +5,66 @@ TrainerPalettes:
 ; only the middle two colors are included, not black or white.
 
 PlayerPalette: ; Chris uses the same colors as Cal
-INCBIN "gfx/trainers/cal.gbcpal", middle_colors
-INCBIN "gfx/trainers/cal.gbcpal", middle_colors
-INCBIN "gfx/trainers/scientist.gbcpal", middle_colors
-INCBIN "gfx/trainers/youngster.gbcpal", middle_colors
-INCBIN "gfx/trainers/schoolboy.gbcpal", middle_colors
-INCBIN "gfx/trainers/bird_keeper.gbcpal", middle_colors
-INCBIN "gfx/trainers/lass.gbcpal", middle_colors
-INCBIN "gfx/trainers/cooltrainer_m.gbcpal", middle_colors
-INCBIN "gfx/trainers/cooltrainer_f.gbcpal", middle_colors
-INCBIN "gfx/trainers/beauty.gbcpal", middle_colors
-INCBIN "gfx/trainers/pokemaniac.gbcpal", middle_colors
-INCBIN "gfx/trainers/grunt_m.gbcpal", middle_colors
-INCBIN "gfx/trainers/gentleman.gbcpal", middle_colors
-INCBIN "gfx/trainers/skier.gbcpal", middle_colors
-INCBIN "gfx/trainers/teacher.gbcpal", middle_colors
-INCBIN "gfx/trainers/bug_catcher.gbcpal", middle_colors
-INCBIN "gfx/trainers/fisher.gbcpal", middle_colors
-INCBIN "gfx/trainers/swimmer_m.gbcpal", middle_colors
-INCBIN "gfx/trainers/swimmer_f.gbcpal", middle_colors
-INCBIN "gfx/trainers/sailor.gbcpal", middle_colors
-INCBIN "gfx/trainers/super_nerd.gbcpal", middle_colors
-INCBIN "gfx/trainers/guitarist.gbcpal", middle_colors
-INCBIN "gfx/trainers/hiker.gbcpal", middle_colors
-INCBIN "gfx/trainers/biker.gbcpal", middle_colors
-INCBIN "gfx/trainers/burglar.gbcpal", middle_colors
-INCBIN "gfx/trainers/firebreather.gbcpal", middle_colors
-INCBIN "gfx/trainers/juggler.gbcpal", middle_colors
-INCBIN "gfx/trainers/blackbelt_t.gbcpal", middle_colors
-INCBIN "gfx/trainers/psychic_t.gbcpal", middle_colors
-INCBIN "gfx/trainers/picnicker.gbcpal", middle_colors
-INCBIN "gfx/trainers/camper.gbcpal", middle_colors
-INCBIN "gfx/trainers/sage.gbcpal", middle_colors
-INCBIN "gfx/trainers/medium.gbcpal", middle_colors
-INCBIN "gfx/trainers/boarder.gbcpal", middle_colors
-INCBIN "gfx/trainers/pokefan_m.gbcpal", middle_colors
-INCBIN "gfx/trainers/kimono_girl.gbcpal", middle_colors
-INCBIN "gfx/trainers/twins.gbcpal", middle_colors
-INCBIN "gfx/trainers/pokefan_f.gbcpal", middle_colors
-INCBIN "gfx/trainers/red.gbcpal", middle_colors
-INCBIN "gfx/trainers/officer.gbcpal", middle_colors
-INCBIN "gfx/trainers/grunt_f.gbcpal", middle_colors
-INCBIN "gfx/trainers/azure.gbcpal", middle_colors
-INCBIN "gfx/trainers/larry.gbcpal", middle_colors
-INCBIN "gfx/trainers/green.gbcpal", middle_colors
-INCBIN "gfx/trainers/robored.gbcpal", middle_colors
-INCBIN "gfx/trainers/chris.gbcpal", middle_colors
-INCBIN "gfx/trainers/kris.gbcpal", middle_colors
-INCBIN "gfx/trainers/evan.gbcpal", middle_colors
-INCBIN "gfx/trainers/chris-snow.gbcpal", middle_colors
-INCBIN "gfx/trainers/brendan.gbcpal", middle_colors
-INCBIN "gfx/trainers/leaf.gbcpal", middle_colors
-INCBIN "gfx/trainers/wes.gbcpal", middle_colors
-INCBIN "gfx/trainers/michael.gbcpal", middle_colors
-INCBIN "gfx/trainers/lucas.gbcpal", middle_colors
-INCBIN "gfx/trainers/lyra.gbcpal", middle_colors
-INCBIN "gfx/trainers/nate.gbcpal", middle_colors
-INCBIN "gfx/trainers/serena2.gbcpal", middle_colors
-INCBIN "gfx/trainers/elio.gbcpal", middle_colors
-INCBIN "gfx/trainers/glazed.gbcpal", middle_colors
-INCBIN "gfx/trainers/nuzlocke.gbcpal", middle_colors
+INCBIN "gfx/trainers/cal.gbcpal", middle_colors ;PLAYER
+INCBIN "gfx/trainers/cal.gbcpal", middle_colors ;CAL
+INCBIN "gfx/trainers/scientist.gbcpal", middle_colors ;SCIENTIST
+INCBIN "gfx/trainers/youngster.gbcpal", middle_colors ;YOUNGSTER
+INCBIN "gfx/trainers/schoolboy.gbcpal", middle_colors ;SCHOOLBOY
+INCBIN "gfx/trainers/bird_keeper.gbcpal", middle_colors ;BIRD_KEEPER
+INCBIN "gfx/trainers/lass.gbcpal", middle_colors ;LASS
+INCBIN "gfx/trainers/cooltrainer_m.gbcpal", middle_colors ;COOLTRAINERM
+INCBIN "gfx/trainers/cooltrainer_f.gbcpal", middle_colors ;COOLTRAINERF
+INCBIN "gfx/trainers/beauty.gbcpal", middle_colors ;BEAUTY
+INCBIN "gfx/trainers/pokemaniac.gbcpal", middle_colors ;POKEMANIAC
+INCBIN "gfx/trainers/grunt_m.gbcpal", middle_colors ;GRUNTM
+INCBIN "gfx/trainers/gentleman.gbcpal", middle_colors ;GENTLEMAN
+INCBIN "gfx/trainers/skier.gbcpal", middle_colors ;SKIER
+INCBIN "gfx/trainers/teacher.gbcpal", middle_colors ;TEACHER
+INCBIN "gfx/trainers/bug_catcher.gbcpal", middle_colors ;BUG_CATCHER
+INCBIN "gfx/trainers/fisher.gbcpal", middle_colors ;FISHER
+INCBIN "gfx/trainers/swimmer_m.gbcpal", middle_colors ;SWIMMERM
+INCBIN "gfx/trainers/swimmer_f.gbcpal", middle_colors ;SWIMMERF
+INCBIN "gfx/trainers/sailor.gbcpal", middle_colors ;SAILOR
+INCBIN "gfx/trainers/super_nerd.gbcpal", middle_colors ;SUPER_NERD
+INCBIN "gfx/trainers/guitarist.gbcpal", middle_colors ;GUITARIST
+INCBIN "gfx/trainers/hiker.gbcpal", middle_colors ;HIKER
+INCBIN "gfx/trainers/biker.gbcpal", middle_colors ;BIKER
+INCBIN "gfx/trainers/burglar.gbcpal", middle_colors ;BURGLAR
+INCBIN "gfx/trainers/firebreather.gbcpal", middle_colors ;FIREBREATHER
+INCBIN "gfx/trainers/juggler.gbcpal", middle_colors ;JUGGLER
+INCBIN "gfx/trainers/blackbelt_t.gbcpal", middle_colors ;BLACKBELT_T
+INCBIN "gfx/trainers/psychic_t.gbcpal", middle_colors ;PSYCHIC_T
+INCBIN "gfx/trainers/picnicker.gbcpal", middle_colors ;PICNICKER
+INCBIN "gfx/trainers/camper.gbcpal", middle_colors ;CAMPER
+INCBIN "gfx/trainers/sage.gbcpal", middle_colors ;SAGE
+INCBIN "gfx/trainers/medium.gbcpal", middle_colors ;MEDIUM
+INCBIN "gfx/trainers/boarder.gbcpal", middle_colors ;BOARDER
+INCBIN "gfx/trainers/pokefan_m.gbcpal", middle_colors ;POKEFANM
+INCBIN "gfx/trainers/kimono_girl.gbcpal", middle_colors ;KIMONO_GIRL
+INCBIN "gfx/trainers/twins.gbcpal", middle_colors ;TWINS
+INCBIN "gfx/trainers/pokefan_f.gbcpal", middle_colors ;POKEFANF
+INCBIN "gfx/trainers/red.gbcpal", middle_colors ;RED
+INCBIN "gfx/trainers/officer.gbcpal", middle_colors ;OFFICER
+INCBIN "gfx/trainers/grunt_f.gbcpal", middle_colors ;GRUNTF
+INCBIN "gfx/trainers/azure.gbcpal", middle_colors ;AZURE
+INCBIN "gfx/trainers/larry.gbcpal", middle_colors ;FC_LARRY
+INCBIN "gfx/trainers/green.gbcpal", middle_colors ;RBY_GREEN
+INCBIN "gfx/trainers/robored.gbcpal", middle_colors ;CY_ROBORED
+INCBIN "gfx/trainers/chris.gbcpal", middle_colors ;GSCHGSS_CHRIS
+INCBIN "gfx/trainers/kris.gbcpal", middle_colors ;GSC_KRIS
+INCBIN "gfx/trainers/evan.gbcpal", middle_colors ;AC_CHRIS
+INCBIN "gfx/trainers/chris-snow.gbcpal", middle_colors ;FC_CHRIS
+INCBIN "gfx/trainers/brendan.gbcpal", middle_colors ;BRENDAN_KANTO_LEADER
+INCBIN "gfx/trainers/leaf.gbcpal", middle_colors ;FRLG_LEAF
+INCBIN "gfx/trainers/wes.gbcpal", middle_colors ;COLO_WES
+INCBIN "gfx/trainers/michael.gbcpal", middle_colors ;XD_MICHAEL
+INCBIN "gfx/trainers/lucas.gbcpal", middle_colors ;PT_LUCAS
+INCBIN "gfx/trainers/lyra.gbcpal", middle_colors ;HGSS_LYRA
+INCBIN "gfx/trainers/nate.gbcpal", middle_colors ;B2W2_NATE
+INCBIN "gfx/trainers/serena2.gbcpal", middle_colors ;XY_SERENA2
+INCBIN "gfx/trainers/elio.gbcpal", middle_colors ;SMUSUM_ELIO
+INCBIN "gfx/trainers/glazed.gbcpal", middle_colors ;GLAZED
+INCBIN "gfx/trainers/nuzlocke.gbcpal", middle_colors 
 INCBIN "gfx/trainers/cyan.gbcpal", middle_colors
 INCBIN "gfx/trainers/red-snow.gbcpal", middle_colors
 INCBIN "gfx/trainers/greenmay.gbcpal", middle_colors

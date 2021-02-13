6053c1eed5f165659e7693129573ac7b97ada0a9
M4 <dave.was.r@gmail.com>
2021-02-13 07:10:44 -0500

    Audited frame pointers #6

--- gfx/pokemon/frame_pointers.asm
+++ gfx/pokemon/frame_pointers.asm
@@ -253,12 +253,12 @@ FramesPointers:
 	dba TreeckoFrames
 	dba GrovyleFrames
 	dba SceptileFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Treecko
 	dba CombuskenFrames
 	dba BlazikenFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Mudkip
+	dba PlaceholderFrames ; PLACEHOLDER - Marshtomp
+	dba PlaceholderFrames ; PLACEHOLDER - Swampert
 	dba PoochyenaFrames
 	dba MightyenaFrames
 	dba ZigzagoonFrames
@@ -269,133 +269,135 @@ FramesPointers:
 	dba TaillowFrames
 	dba SwellowFrames
 	dba WingullFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Pelipper
 	dba RaltsFrames
 	dba KirliaFrames
 	dba GardevoirFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Surskit
+	dba PlaceholderFrames ; PLACEHOLDER - Masquerain
 	dba WhismurFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Loudred
+	dba PlaceholderFrames ; PLACEHOLDER - Exploud
+	dba PlaceholderFrames ; PLACEHOLDER - Makuhita
+	dba PlaceholderFrames ; PLACEHOLDER - Hariyama
+	dba PlaceholderFrames ; PLACEHOLDER - Azurill
+	dba PlaceholderFrames ; PLACEHOLDER - Skitty
+	dba PlaceholderFrames ; PLACEHOLDER - Delcatty
+	dba PlaceholderFrames ; PLACEHOLDER - Sableye
+	dba PlaceholderFrames ; PLACEHOLDER - Aron
+	dba PlaceholderFrames ; PLACEHOLDER - Lairon
+	dba PlaceholderFrames ; PLACEHOLDER - Aggron
+	dba PlaceholderFrames ; PLACEHOLDER - Electrike
+	dba PlaceholderFrames ; PLACEHOLDER - Manectric
+	dba PlaceholderFrames ; PLACEHOLDER - Roselia
+	dba PlaceholderFrames ; PLACEHOLDER - Wailmer
+	dba PlaceholderFrames ; PLACEHOLDER - Wailord
+	dba PlaceholderFrames ; PLACEHOLDER - Trapinch
+	dba PlaceholderFrames ; PLACEHOLDER - Vibrava
+	dba PlaceholderFrames ; PLACEHOLDER - Flygon
+	dba PlaceholderFrames ; PLACEHOLDER - Swablu
+	dba PlaceholderFrames ; PLACEHOLDER - Altaira
+	dba PlaceholderFrames ; PLACEHOLDER - Lileep
+	dba PlaceholderFrames ; PLACEHOLDER - Cradily
+	dba PlaceholderFrames ; PLACEHOLDER - Feebas
+	dba PlaceholderFrames ; PLACEHOLDER - Milotic
+	dba PlaceholderFrames ; PLACEHOLDER - Duskull
+	dba PlaceholderFrames ; PLACEHOLDER - Dusclops
+	dba PlaceholderFrames ; PLACEHOLDER - Absol
+	dba PlaceholderFrames ; PLACEHOLDER - Snorunt
+	dba PlaceholderFrames ; PLACEHOLDER - Spheal
+	dba PlaceholderFrames ; PLACEHOLDER - Sealeo
+	dba PlaceholderFrames ; PLACEHOLDER - Walrein
+	dba PlaceholderFrames ; PLACEHOLDER - Bagon
+	dba PlaceholderFrames ; PLACEHOLDER - Shelgon
+	dba PlaceholderFrames ; PLACEHOLDER - Salamence
+	dba PlaceholderFrames ; PLACEHOLDER - Beldum
+	dba PlaceholderFrames ; PLACEHOLDER - Metang
+	dba PlaceholderFrames ; PLACEHOLDER - Metagross
+	dba PlaceholderFrames ; PLACEHOLDER - Latias
+	dba PlaceholderFrames ; PLACEHOLDER - Latios
+	dba PlaceholderFrames ; PLACEHOLDER - Groudon
+	dba PlaceholderFrames ; PLACEHOLDER - Turtwig
+	dba PlaceholderFrames ; PLACEHOLDER - Grotle
+	dba PlaceholderFrames ; PLACEHOLDER - Torterra
+	dba PlaceholderFrames ; PLACEHOLDER - Chimchar
+	dba PlaceholderFrames ; PLACEHOLDER - Monferno
+	dba PlaceholderFrames ; PLACEHOLDER - Infernape
+	dba PlaceholderFrames ; PLACEHOLDER - Piplup
+	dba PlaceholderFrames ; PLACEHOLDER - Prinplup
+	dba PlaceholderFrames ; PLACEHOLDER - Empoleon
+	dba PlaceholderFrames ; PLACEHOLDER - Bidoof
+	dba PlaceholderFrames ; PLACEHOLDER - Bibarel
+	dba PlaceholderFrames ; PLACEHOLDER - Shinx
+	dba PlaceholderFrames ; PLACEHOLDER - Luxio
+	dba PlaceholderFrames ; PLACEHOLDER - Luxray
+	dba PlaceholderFrames ; PLACEHOLDER - Budew
+	dba PlaceholderFrames ; PLACEHOLDER - Roserade
+	dba PlaceholderFrames ; PLACEHOLDER - Burmy
+	dba PlaceholderFrames ; PLACEHOLDER - Wormadam
+	dba PlaceholderFrames ; PLACEHOLDER - Buizel
+	dba PlaceholderFrames ; PLACEHOLDER - Floatzel
+	dba PlaceholderFrames ; PLACEHOLDER - Shellos
+	dba PlaceholderFrames ; PLACEHOLDER - Gastrodon
+	dba PlaceholderFrames ; PLACEHOLDER - Buneary
+	dba PlaceholderFrames ; PLACEHOLDER - Lopunny
+	dba PlaceholderFrames ; PLACEHOLDER - Stunky
+	dba PlaceholderFrames ; PLACEHOLDER - Skuntank
+	dba PlaceholderFrames ; PLACEHOLDER - Bronzor
+	dba PlaceholderFrames ; PLACEHOLDER - Bronzong
+	dba PlaceholderFrames ; PLACEHOLDER - Bonsly
 	dba ChatotFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Croagunk
+	dba PlaceholderFrames ; PLACEHOLDER - Toxicroak
+	dba PlaceholderFrames ; PLACEHOLDER - Leafeon
 	dba GlaceonFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Porygon Z
+	dba PlaceholderFrames ; PLACEHOLDER - Dusknoir
+	dba PlaceholderFrames ; PLACEHOLDER - Froslass
+	dba PlaceholderFrames ; PLACEHOLDER - Darkai
+	dba PlaceholderFrames ; PLACEHOLDER - Pansage
+	dba PlaceholderFrames ; PLACEHOLDER - Simisage
+	dba PlaceholderFrames ; PLACEHOLDER - Drillbur
+	dba PlaceholderFrames ; PLACEHOLDER - Excadrill
+	dba PlaceholderFrames ; PLACEHOLDER - Sandile
+	dba PlaceholderFrames ; PLACEHOLDER - Krokorok
+	dba PlaceholderFrames ; PLACEHOLDER - Krookodile
+	dba PlaceholderFrames ; PLACEHOLDER - Ducklett
+	dba PlaceholderFrames ; PLACEHOLDER - Swanna
+	dba PlaceholderFrames ; PLACEHOLDER - Tynamo
+	dba PlaceholderFrames ; PLACEHOLDER - Eelektrik
+	dba PlaceholderFrames ; PLACEHOLDER - Eelektross
+	dba PlaceholderFrames ; PLACEHOLDER - Litwick
+	dba PlaceholderFrames ; PLACEHOLDER - Lampent
+	dba PlaceholderFrames ; PLACEHOLDER - Chandelure
+	dba PlaceholderFrames ; PLACEHOLDER - Clauncher
+	dba PlaceholderFrames ; PLACEHOLDER - Clawitzer
+	dba PlaceholderFrames ; PLACEHOLDER - Helioptile
+	dba PlaceholderFrames ; PLACEHOLDER - Heliolisk
+	dba PlaceholderFrames ; PLACEHOLDER - Amaura
+	dba PlaceholderFrames ; PLACEHOLDER - Aurorus
+	dba PlaceholderFrames ; PLACEHOLDER - Sylveon
+	dba PlaceholderFrames ; PLACEHOLDER - Goomy
+	dba PlaceholderFrames ; PLACEHOLDER - Sliggoo
+	dba PlaceholderFrames ; PLACEHOLDER - Goodra
+	dba PlaceholderFrames ; PLACEHOLDER - Crabrawler
+	dba PlaceholderFrames ; PLACEHOLDER - Crabominable
+	dba PlaceholderFrames ; PLACEHOLDER - Cutiefly
+	dba PlaceholderFrames ; PLACEHOLDER - Ribombee
+	; Fused Crystal mons
+	dba PlaceholderFrames ; PLACEHOLDER - Onixtret
+	dba PlaceholderFrames ; PLACEHOLDER - Chiquirtle
+	dba PlaceholderFrames ; PLACEHOLDER - Baytortle
 	dba MegastoiseFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Hootduo
 	dba NoctdrioFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Togekey
 	dba TogetapeFrames
-	dba PlaceholderFrames ; PLACEHOLDER
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Hopporita
+	dba PlaceholderFrames ; PLACEHOLDER - Skipleef
 	dba JumpaniumFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	dba PlaceholderFrames ; PLACEHOLDER - Woochum
 	dba QuagynxFrames
 	dba SteelurretFrames
-	dba PlaceholderFrames ; PLACEHOLDER
+	; TPP's signature fakemon
+	dba PlaceholderFrames ; PLACEHOLDER - Phancero

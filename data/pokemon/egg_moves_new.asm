20931b3ec501301fc9ac483730960d2f66bfcdf6
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-13 03:32:38 -0300

    Avoid midline tabs

--- data/pokemon/egg_moves_new.asm
+++ data/pokemon/egg_moves_new.asm
@@ -2,156 +2,156 @@ SECTION "Egg Moves 3", ROMX
 
 EggMovePointers3::
 	dw TreeckoEggMoves
-	dw NoEggMoves3			; Grovyle
-	dw NoEggMoves3			; Sceptile
-	dw NoEggMoves3 			; PLACEHOLDER - Torchic
-	dw NoEggMoves3			; Combusken
-	dw NoEggMoves3 			; Blaziken
-	dw NoEggMoves3 			; PLACEHOLDER - Mudkip
-	dw NoEggMoves3 			; Marshtomp
-	dw NoEggMoves3 			; Swampert
+	dw NoEggMoves3         ; Grovyle
+	dw NoEggMoves3         ; Sceptile
+	dw NoEggMoves3         ; PLACEHOLDER - Torchic
+	dw NoEggMoves3         ; Combusken
+	dw NoEggMoves3         ; Blaziken
+	dw NoEggMoves3         ; PLACEHOLDER - Mudkip
+	dw NoEggMoves3         ; Marshtomp
+	dw NoEggMoves3         ; Swampert
 	dw PoochyenaEggMoves
-	dw NoEggMoves3			; Mightyena
+	dw NoEggMoves3         ; Mightyena
 	dw ZigzagoonEggMoves
-	dw NoEggMoves3			; Linoone
-	dw NoEggMoves3 			; PLACEHOLDER - Lotad
-	dw NoEggMoves3			; Lombre
-	dw NoEggMoves3			; Ludicolo
-	dw NoEggMoves3 			; PLACEHOLDER - Taillow
-	dw NoEggMoves3 			; Swellow
+	dw NoEggMoves3         ; Linoone
+	dw NoEggMoves3         ; PLACEHOLDER - Lotad
+	dw NoEggMoves3         ; Lombre
+	dw NoEggMoves3         ; Ludicolo
+	dw NoEggMoves3         ; PLACEHOLDER - Taillow
+	dw NoEggMoves3         ; Swellow
 	dw WingullEggMoves
-	dw NoEggMoves3 			; Pelipper
-	dw NoEggMoves3 			; PLACEHOLDER - Ralts
-	dw NoEggMoves3 			; Kirlia
-	dw NoEggMoves3 			; Gardevoir
-	dw NoEggMoves3 			; PLACEHOLDER - Surskit
-	dw NoEggMoves3 			; Masquerain
+	dw NoEggMoves3         ; Pelipper
+	dw NoEggMoves3         ; PLACEHOLDER - Ralts
+	dw NoEggMoves3         ; Kirlia
+	dw NoEggMoves3         ; Gardevoir
+	dw NoEggMoves3         ; PLACEHOLDER - Surskit
+	dw NoEggMoves3         ; Masquerain
 	dw WhismurEggMoves
-	dw NoEggMoves3 			; Loudred
-	dw NoEggMoves3 			; Exploud
-	dw NoEggMoves3 			; PLACEHOLDER - Makuhita
-	dw NoEggMoves3 			; Hariyama
+	dw NoEggMoves3         ; Loudred
+	dw NoEggMoves3         ; Exploud
+	dw NoEggMoves3         ; PLACEHOLDER - Makuhita
+	dw NoEggMoves3         ; Hariyama
 	dw AzurillEggMoves
-	dw NoEggMoves3 			; PLACEHOLDER - Skitty
-	dw NoEggMoves3 			; Delcatty
-	dw NoEggMoves3 			; PLACEHOLDER - Sableye
-	dw NoEggMoves3 			; PLACEHOLDER - Aron
-	dw NoEggMoves3 			; Lairon
-	dw NoEggMoves3 			; Aggron
-	dw NoEggMoves3 			; PLACEHOLDER - Electrike
-	dw NoEggMoves3 			; Manectric
-	dw NoEggMoves3 			; Roselia
-	dw NoEggMoves3 			; PLACEHOLDER - Wailmer
-	dw NoEggMoves3 			; Wailord
-	dw NoEggMoves3 			; PLACEHOLDER - Trapinch
-	dw NoEggMoves3 			; Vibrava
-	dw NoEggMoves3 			; Flygon
-	dw NoEggMoves3 			; PLACEHOLDER - Swablu
-	dw NoEggMoves3 			; Altaira
-	dw NoEggMoves3 			; PLACEHOLDER - Lileep
-	dw NoEggMoves3 			; Cradily
-	dw NoEggMoves3 			; PLACEHOLDER - Feebas
-	dw NoEggMoves3 			; Milotic
-	dw NoEggMoves3 			; PLACEHOLDER - Duskull
-	dw NoEggMoves3 			; Dusclops
-	dw NoEggMoves3 			; PLACEHOLDER - Absol
-	dw NoEggMoves3 			; PLACEHOLDER - Snorunt
-	dw NoEggMoves3 			; PLACEHOLDER - Spheal
-	dw NoEggMoves3 			; Sealeo
-	dw NoEggMoves3 			; Walrein
-	dw NoEggMoves3 			; PLACEHOLDER - Bagon
-	dw NoEggMoves3 			; Shelgon
-	dw NoEggMoves3 			; Salamence
-	dw NoEggMoves3 			; PLACEHOLDER - Beldum
-	dw NoEggMoves3 			; Metang
-	dw NoEggMoves3 			; Metagross
-	dw NoEggMoves3 			; Latias
-	dw NoEggMoves3 			; Latios
-	dw NoEggMoves3 			; Groudon
-	dw NoEggMoves3 			; PLACEHOLDER - Turtwig
-	dw NoEggMoves3 			; Grotle
-	dw NoEggMoves3 			; Torterra
-	dw NoEggMoves3 			; PLACEHOLDER - Chimchar
-	dw NoEggMoves3 			; Monferno
-	dw NoEggMoves3 			; Infernape
-	dw NoEggMoves3 			; PLACEHOLDER - Piplup
-	dw NoEggMoves3 			; Prinplup
-	dw NoEggMoves3 			; Empoleon
-	dw NoEggMoves3 			; PLACEHOLDER - Bidoof
-	dw NoEggMoves3 			; Bibarel
-	dw NoEggMoves3 			; PLACEHOLDER - Shinx
-	dw NoEggMoves3 			; Luxio
-	dw NoEggMoves3 			; Luxray
-	dw NoEggMoves3 			; PLACEHOLDER - Budew
-	dw NoEggMoves3 			; Roserade
-	dw NoEggMoves3 			; PLACEHOLDER - Burmy
-	dw NoEggMoves3 			; Wormadam
-	dw NoEggMoves3 			; PLACEHOLDER - Buizel
-	dw NoEggMoves3 			; Floatzel
-	dw NoEggMoves3 			; PLACEHOLDER - Shellos
-	dw NoEggMoves3 			; Gastrodon
-	dw NoEggMoves3 			; PLACEHOLDER - Buneary
-	dw NoEggMoves3 			; Lopunny
-	dw NoEggMoves3 			; PLACEHOLDER - Stunky
-	dw NoEggMoves3 			; Skuntank
-	dw NoEggMoves3 			; PLACEHOLDER - Bronzor
-	dw NoEggMoves3 			; Bronzong
+	dw NoEggMoves3         ; PLACEHOLDER - Skitty
+	dw NoEggMoves3         ; Delcatty
+	dw NoEggMoves3         ; PLACEHOLDER - Sableye
+	dw NoEggMoves3         ; PLACEHOLDER - Aron
+	dw NoEggMoves3         ; Lairon
+	dw NoEggMoves3         ; Aggron
+	dw NoEggMoves3         ; PLACEHOLDER - Electrike
+	dw NoEggMoves3         ; Manectric
+	dw NoEggMoves3         ; Roselia
+	dw NoEggMoves3         ; PLACEHOLDER - Wailmer
+	dw NoEggMoves3         ; Wailord
+	dw NoEggMoves3         ; PLACEHOLDER - Trapinch
+	dw NoEggMoves3         ; Vibrava
+	dw NoEggMoves3         ; Flygon
+	dw NoEggMoves3         ; PLACEHOLDER - Swablu
+	dw NoEggMoves3         ; Altaira
+	dw NoEggMoves3         ; PLACEHOLDER - Lileep
+	dw NoEggMoves3         ; Cradily
+	dw NoEggMoves3         ; PLACEHOLDER - Feebas
+	dw NoEggMoves3         ; Milotic
+	dw NoEggMoves3         ; PLACEHOLDER - Duskull
+	dw NoEggMoves3         ; Dusclops
+	dw NoEggMoves3         ; PLACEHOLDER - Absol
+	dw NoEggMoves3         ; PLACEHOLDER - Snorunt
+	dw NoEggMoves3         ; PLACEHOLDER - Spheal
+	dw NoEggMoves3         ; Sealeo
+	dw NoEggMoves3         ; Walrein
+	dw NoEggMoves3         ; PLACEHOLDER - Bagon
+	dw NoEggMoves3         ; Shelgon
+	dw NoEggMoves3         ; Salamence
+	dw NoEggMoves3         ; PLACEHOLDER - Beldum
+	dw NoEggMoves3         ; Metang
+	dw NoEggMoves3         ; Metagross
+	dw NoEggMoves3         ; Latias
+	dw NoEggMoves3         ; Latios
+	dw NoEggMoves3         ; Groudon
+	dw NoEggMoves3         ; PLACEHOLDER - Turtwig
+	dw NoEggMoves3         ; Grotle
+	dw NoEggMoves3         ; Torterra
+	dw NoEggMoves3         ; PLACEHOLDER - Chimchar
+	dw NoEggMoves3         ; Monferno
+	dw NoEggMoves3         ; Infernape
+	dw NoEggMoves3         ; PLACEHOLDER - Piplup
+	dw NoEggMoves3         ; Prinplup
+	dw NoEggMoves3         ; Empoleon
+	dw NoEggMoves3         ; PLACEHOLDER - Bidoof
+	dw NoEggMoves3         ; Bibarel
+	dw NoEggMoves3         ; PLACEHOLDER - Shinx
+	dw NoEggMoves3         ; Luxio
+	dw NoEggMoves3         ; Luxray
+	dw NoEggMoves3         ; PLACEHOLDER - Budew
+	dw NoEggMoves3         ; Roserade
+	dw NoEggMoves3         ; PLACEHOLDER - Burmy
+	dw NoEggMoves3         ; Wormadam
+	dw NoEggMoves3         ; PLACEHOLDER - Buizel
+	dw NoEggMoves3         ; Floatzel
+	dw NoEggMoves3         ; PLACEHOLDER - Shellos
+	dw NoEggMoves3         ; Gastrodon
+	dw NoEggMoves3         ; PLACEHOLDER - Buneary
+	dw NoEggMoves3         ; Lopunny
+	dw NoEggMoves3         ; PLACEHOLDER - Stunky
+	dw NoEggMoves3         ; Skuntank
+	dw NoEggMoves3         ; PLACEHOLDER - Bronzor
+	dw NoEggMoves3         ; Bronzong
 	dw BonslyEggMoves 
 	dw ChatotEggMoves
-	dw NoEggMoves3 			; PLACEHOLDER - Croagunk
-	dw NoEggMoves3 			; Toxicroak
-	dw NoEggMoves3 			; Leafon
-	dw NoEggMoves3 			; Glaceon
-	dw NoEggMoves3 			; Porygon Z
-	dw NoEggMoves3 			; Dusknoir
-	dw NoEggMoves3 			; Froslass
-	dw NoEggMoves3 			; Darkrai
-	dw NoEggMoves3 			; PLACEHOLDER - Pansage
-	dw NoEggMoves3 			; Simisage
-	dw NoEggMoves3 			; PLACEHOLDER - Drillbur
-	dw NoEggMoves3 			; Excadrill
-	dw NoEggMoves3 			; PLACEHOLDER - Sandile
-	dw NoEggMoves3 			; Krokorok
-	dw NoEggMoves3 			; Krookodile
-	dw NoEggMoves3 			; PLACEHOLDER - Ducklett
-	dw NoEggMoves3 			; Swanna
-	dw NoEggMoves3 			; PLACEHOLDER - Tynamo
-	dw NoEggMoves3 			; Eelektrik
-	dw NoEggMoves3 			; Eelektross
-	dw NoEggMoves3 			; PLACEHOLDER - Litwick
-	dw NoEggMoves3 			; Lampent
-	dw NoEggMoves3 			; Chandelure
-	dw NoEggMoves3 			; PLACEHOLDER - Clauncher
-	dw NoEggMoves3 			; Clawitzer
-	dw NoEggMoves3 			; PLACEHOLDER - Helioptile
-	dw NoEggMoves3 			; Heliolisk
-	dw NoEggMoves3 			; PLACEHOLDER - Amaura
-	dw NoEggMoves3 			; Aurorus
-	dw NoEggMoves3 			; Sylveon
-	dw NoEggMoves3 			; PLACEHOLDER - Goomy
-	dw NoEggMoves3 			; Sliggoo
-	dw NoEggMoves3 			; Goodra
-	dw NoEggMoves3 			; PLACEHOLDER - Crabrawler
-	dw NoEggMoves3 			; Crabominable
-	dw NoEggMoves3 			; PLACEHOLDER - Cutiefly
-	dw NoEggMoves3 			; Ribombee
+	dw NoEggMoves3         ; PLACEHOLDER - Croagunk
+	dw NoEggMoves3         ; Toxicroak
+	dw NoEggMoves3         ; Leafon
+	dw NoEggMoves3         ; Glaceon
+	dw NoEggMoves3         ; Porygon Z
+	dw NoEggMoves3         ; Dusknoir
+	dw NoEggMoves3         ; Froslass
+	dw NoEggMoves3         ; Darkrai
+	dw NoEggMoves3         ; PLACEHOLDER - Pansage
+	dw NoEggMoves3         ; Simisage
+	dw NoEggMoves3         ; PLACEHOLDER - Drillbur
+	dw NoEggMoves3         ; Excadrill
+	dw NoEggMoves3         ; PLACEHOLDER - Sandile
+	dw NoEggMoves3         ; Krokorok
+	dw NoEggMoves3         ; Krookodile
+	dw NoEggMoves3         ; PLACEHOLDER - Ducklett
+	dw NoEggMoves3         ; Swanna
+	dw NoEggMoves3         ; PLACEHOLDER - Tynamo
+	dw NoEggMoves3         ; Eelektrik
+	dw NoEggMoves3         ; Eelektross
+	dw NoEggMoves3         ; PLACEHOLDER - Litwick
+	dw NoEggMoves3         ; Lampent
+	dw NoEggMoves3         ; Chandelure
+	dw NoEggMoves3         ; PLACEHOLDER - Clauncher
+	dw NoEggMoves3         ; Clawitzer
+	dw NoEggMoves3         ; PLACEHOLDER - Helioptile
+	dw NoEggMoves3         ; Heliolisk
+	dw NoEggMoves3         ; PLACEHOLDER - Amaura
+	dw NoEggMoves3         ; Aurorus
+	dw NoEggMoves3         ; Sylveon
+	dw NoEggMoves3         ; PLACEHOLDER - Goomy
+	dw NoEggMoves3         ; Sliggoo
+	dw NoEggMoves3         ; Goodra
+	dw NoEggMoves3         ; PLACEHOLDER - Crabrawler
+	dw NoEggMoves3         ; Crabominable
+	dw NoEggMoves3         ; PLACEHOLDER - Cutiefly
+	dw NoEggMoves3         ; Ribombee
 	; Fused Crystal mons
-	dw NoEggMoves3 			; PLACEHOLDER - Onixtret
-	dw NoEggMoves3 			; PLACEHOLDER - Chiquirtle
-	dw NoEggMoves3 			; Baytortle
-	dw NoEggMoves3			; Megastoise
-	dw NoEggMoves3 			; PLACEHOLDER - Hootduo
-	dw NoEggMoves3			; Noctdrio
-	dw NoEggMoves3 			; PLACEHOLDER - Togekey
-	dw NoEggMoves3			; Togetape
-	dw NoEggMoves3 			; PLACEHOLDER - Hopporita
-	dw NoEggMoves3 			; Skipleef
-	dw NoEggMoves3			; Jumpanium
-	dw NoEggMoves3			; PLACEHOLDER - Woochum
-	dw NoEggMoves3			; Quagynx
-	dw NoEggMoves3			; Steelurret
+	dw NoEggMoves3         ; PLACEHOLDER - Onixtret
+	dw NoEggMoves3         ; PLACEHOLDER - Chiquirtle
+	dw NoEggMoves3         ; Baytortle
+	dw NoEggMoves3         ; Megastoise
+	dw NoEggMoves3         ; PLACEHOLDER - Hootduo
+	dw NoEggMoves3         ; Noctdrio
+	dw NoEggMoves3         ; PLACEHOLDER - Togekey
+	dw NoEggMoves3         ; Togetape
+	dw NoEggMoves3         ; PLACEHOLDER - Hopporita
+	dw NoEggMoves3         ; Skipleef
+	dw NoEggMoves3         ; Jumpanium
+	dw NoEggMoves3         ; PLACEHOLDER - Woochum
+	dw NoEggMoves3         ; Quagynx
+	dw NoEggMoves3         ; Steelurret
 	; TPP's signature fakemon
-	dw NoEggMoves3 			; Phancero
+	dw NoEggMoves3         ; Phancero
 
 TreeckoEggMoves:
 	dw CRUNCH

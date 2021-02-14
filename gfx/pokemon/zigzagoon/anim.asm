272dcefad9d420bfbe550c65fbc5d64c652cee67
red031000 <rubenru09@aol.com>
2021-02-14 21:32:56 +0000

    fix zig animation

--- gfx/pokemon/zigzagoon/anim.asm
+++ gfx/pokemon/zigzagoon/anim.asm
@@ -1,13 +1,15 @@
 	frame 0, 06
 	frame 1, 06
-	frame 2, 06
-	frame 3, 06
-	frame 4, 06
-	frame 2, 06
-	frame 3, 06
-	frame 4, 12
+	frame 2, 03
+	frame 3, 03
+	frame 4, 03
+	frame 5, 03
+	frame 2, 03
+	frame 3, 03
+	frame 4, 03
+	frame 5, 03
 	setrepeat 2
-	frame 5, 06
+	frame 6, 06
 	frame 0, 06
-	dorepeat 9
+	dorepeat 11
 	endanim

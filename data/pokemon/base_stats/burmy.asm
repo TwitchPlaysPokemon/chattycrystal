d191a03d7e3f8aa30b8502623a45b56ad2c64114
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-15 18:31:39 -0300

    All Burmy are male

--- data/pokemon/base_stats/burmy.asm
+++ data/pokemon/base_stats/burmy.asm
@@ -7,7 +7,7 @@
 	db 120 ; catch rate
 	db 61 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F50 ; gender ratio
+	db GENDER_F0 ; gender ratio
 	db 100 ; unknown 1
 	db 16 ; step cycles to hatch
 	db 5 ; unknown 2

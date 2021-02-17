5fb10c46a4c3bfb420a5c05fba9f45ff20adc0ef
Fredrik Ljungdahl <fredde1994@gmail.com>
2021-02-17 19:52:45 +0100

    Don't allow wildmons to steal items

--- engine/battle/move_effects/thief.asm
+++ engine/battle/move_effects/thief.asm
@@ -1,26 +1,21 @@
 BattleCommand_Thief:
-; thief
-
 	ldh a, [hBattleTurn]
 	and a
 	jr nz, .enemy
 
-; The player needs to be able to steal an item.
-
+	; The player needs to be able to steal an item.
 	call .playeritem
 	ld a, [hl]
 	and a
 	ret nz
 
-; The enemy needs to have an item to steal.
-
+	; The enemy needs to have an item to steal.
 	call .enemyitem
 	ld a, [hl]
 	and a
 	ret z
 
-; Can't steal mail.
-
+	; Can't steal mail.
 	ld [wNamedObjectIndexBuffer], a
 	ld d, a
 	farcall ItemIsMail
@@ -51,23 +46,24 @@ BattleCommand_Thief:
 	jr .stole
 
 .enemy
+	; Don't allow wildmons to steal items
+	ld a, [wBattleMode]
+	dec a
+	ret z
 
-; The enemy can't already have an item.
-
+	; The enemy can't already have an item.
 	call .enemyitem
 	ld a, [hl]
 	and a
 	ret nz
 
-; The player must have an item to steal.
-
+	; The player must have an item to steal.
 	call .playeritem
 	ld a, [hl]
 	and a
 	ret z
 
-; Can't steal mail!
-
+	; Can't steal mail!
 	ld [wNamedObjectIndexBuffer], a
 	ld d, a
 	farcall ItemIsMail
@@ -77,9 +73,8 @@ BattleCommand_Thief:
 	and a
 	ret nz
 
-; If the enemy steals your item,
-; it's gone for good if you don't get it back.
-
+	; If the enemy steals your item,
+	; it's gone for good if you don't get it back.
 	call .playeritem
 	xor a
 	ld [hl], a

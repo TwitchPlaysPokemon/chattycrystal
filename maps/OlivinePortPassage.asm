552a4eeddbb5fd1ad711d66a107eb2c076c361ee
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-18 22:11:53 -0300

    Pick up the trash

--- maps/OlivinePortPassage.asm
+++ maps/OlivinePortPassage.asm
@@ -8,25 +8,6 @@ OlivinePortPassage_MapScripts:
 
 OlivinePortPassagePokefanMScript:
 	jumptextfaceplayer OlivinePortPassagePokefanMText
-	
-ShoelaceSnorlax:
-	chattyoff
-	opentext
-	writetext ShoelaceSnorlaxText
-	waitbutton
-	closetext
-	chattyon
-	end
-	
-ShoelaceSnorlaxText:
-	text "It's a SNORLAX"
-	line "tying its"
-	cont "shoelaces"
-	
-	para "You'll have to"
-	line "wait until it is"
-	cont "done<...>"
-	done
 
 OlivinePortPassagePokefanMText:
 	text "FAST SHIP S.S.AQUA"
@@ -50,6 +31,5 @@ OlivinePortPassage_MapEvents:
 
 	db 0 ; bg events
 
-	db 2 ; object events
+	db 1 ; object events
 	object_event 17,  1, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivinePortPassagePokefanMScript, EVENT_OLIVINE_PORT_PASSAGE_POKEFAN_M
-	object_event 15, 3, SPRITE_BIG_SNORLAX, SPRITEMOVEDATA_BIGDOLLSYM, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ShoelaceSnorlax, EVENT_ROUTE_5_SNORLAX

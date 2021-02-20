a2749e9edb630780380812e19729c004eb8979c4
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-20 18:40:51 -0300

    Make healing pad work

--- data/maps/scenes.asm
+++ data/maps/scenes.asm
@@ -87,4 +87,5 @@ MapScenes::
 	scene_var ROUTE_35_GOLDENROD_GATE,                     wRoute35GoldenrodGateSceneID
 	scene_var PEWTER_MUSEUM_1F,                            wPewterMuseum1FSceneID
 	scene_var POKEMON_TOWER_2F,                            wPokemonTower2FSceneID
+	scene_var POKEMON_TOWER_5F,                            wPokemonTower5FSceneID
 	db -1 ; end

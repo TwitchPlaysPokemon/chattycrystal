73072b4de16a79978b2c21dd9cffddba82a678d3
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-21 05:02:48 -0300

    Add Darkrai reset to reset script

--- engine/events/reset_unique_mons.asm
+++ engine/events/reset_unique_mons.asm
@@ -2,6 +2,7 @@ ResetUniqueMons::
 	clearifuncaught HO_OH, EVENT_FOUGHT_HO_OH
 	clearifuncaught LUGIA, EVENT_FOUGHT_LUGIA
 	clearifuncaught LATIOS, EVENT_LATIOS_IN_SAFARI_ZONE
+	clearifuncaught DARKRAI, EVENT_DARKRAI_IN_POKEMON_TOWER
 	; TODO: Snorlax, Entei, Raikou, Suicune, Celebi, and one of the Snorlax
 	; TODO: any unique mons we add
 	end

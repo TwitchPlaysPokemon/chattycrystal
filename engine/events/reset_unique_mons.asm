1c7ce8049e7594e0e15e0515f73b115a8a44b7b0
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-21 05:50:36 -0300

    Fix comment

--- engine/events/reset_unique_mons.asm
+++ engine/events/reset_unique_mons.asm
@@ -3,6 +3,6 @@ ResetUniqueMons::
 	clearifuncaught LUGIA, EVENT_FOUGHT_LUGIA
 	clearifuncaught LATIOS, EVENT_LATIOS_IN_SAFARI_ZONE
 	clearifuncaught DARKRAI, EVENT_DARKRAI_IN_POKEMON_TOWER
-	; TODO: Snorlax, Entei, Raikou, Suicune, Celebi, and one of the Snorlax
+	; TODO: Entei, Raikou, Suicune, Celebi, and one of the Snorlax
 	; TODO: any unique mons we add
 	end

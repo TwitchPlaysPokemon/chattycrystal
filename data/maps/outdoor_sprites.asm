1cf2cb4494a2623602422adb69b3b3937865f2e9
chaos <dd4791.mariokarter@hotmail.co.uk>
2021-02-21 01:31:18 +0000

    fix broken sprite on bridge rocket

--- data/maps/outdoor_sprites.asm
+++ data/maps/outdoor_sprites.asm
@@ -68,7 +68,7 @@ SaffronGroupSprites:
 	db SPRITE_YOUNGSTER
 	db SPRITE_LASS
 	db SPRITE_POKEFAN_M
-	db SPRITE_ROCKET
+	db SPRITE_WES
 	db SPRITE_MISTY
 	; max 9 of 9 walking sprites
 	db SPRITE_BIG_SNORLAX

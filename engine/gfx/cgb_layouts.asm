022d736b9c3ce1c15589f3e585ed340a25af7395
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-02-23 08:16:15 -0300

    Fix Starmie

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -269,6 +269,10 @@ _CGB_Pokedex:
 
 .is_pokemon
 	call GetMonPalettePointer
+	inc hl
+	inc hl
+	inc hl
+	inc hl
 	call LoadPalette_White_Col1_Col2_Black ; mon palette
 .got_palette
 	call WipeAttrMap

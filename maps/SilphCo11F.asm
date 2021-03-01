520daa2ecca52ae984d4f0787d4ff5935223ea3c
m4-used-rollout <dave.was.r@gmail.com>
2021-03-01 05:20:16 +0000

    Moved warps down onto correct tiles

--- maps/SilphCo11F.asm
+++ maps/SilphCo11F.asm
@@ -10,9 +10,9 @@ SilphCo11F_MapEvents:
 	db 0, 0 ; filler
 
 	db 3 ; warp events
-	warp_event  0,  0, SILPH_CO_ELEVATOR, 2
-	warp_event  3,  0, SILPH_CO_ROOF, 1
-	warp_event 13,  0, SILPH_CO_10F, 2
+	warp_event  0,  1, SILPH_CO_ELEVATOR, 2
+	warp_event  3,  1, SILPH_CO_ROOF, 1
+	warp_event 13,  1, SILPH_CO_10F, 2
 
 	db 0 ; coord events
 

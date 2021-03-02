73cf05d8d64cf19c664b1442ccfa721503b9e8ce
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-01 21:26:12 -0300

    More cleanup

--- maps/SilverCaveRoom3.asm
+++ maps/SilverCaveRoom3.asm
@@ -1,7 +1,7 @@
 	object_const_def ; object_event constants
 	const SILVERCAVEROOM3_RED
 
-SilverCaveRoom3_MapScripts: ; 0x18c601
+SilverCaveRoom3_MapScripts:
 	db 0 ; scene scripts
 
 	; callback count
@@ -67,25 +67,20 @@ RedLeavesText:
 	line "<……>"
 	done
 
-SilverCaveRoom3_MapEvents: ; 0x18c644
+SilverCaveRoom3_MapEvents:
 	; filler
 	db 0, 0
 
-	; warps
-	db 3
-	warp_def $21, $9, 2, SILVER_CAVE_ROOM_2
-	warp_def 7, 9, 1, MT_SILVER_RUINS
-	warp_def 7, 10, 2, MT_SILVER_RUINS
+	db 3 ; warp events
+	warp_event  9, 33, SILVER_CAVE_ROOM_2, 2
+	warp_event  9,  7, MT_SILVER_RUINS, 1
+	warp_event 10,  7, MT_SILVER_RUINS, 2
 
-	; xy triggers
-	db 0
+	db 0 ; coord events
 
-	; signposts
-	db 2
-	bg_event 8, 8, BGEVENT_READ, RuinsEntranceSignScript
-	bg_event 11, 8, BGEVENT_READ, RuinsEntranceSignScript
+	db 2 ; bg events
+	bg_event  8,  8, BGEVENT_READ, RuinsEntranceSignScript
+	bg_event 11,  8, BGEVENT_READ, RuinsEntranceSignScript
 
-	; object_event
-	db 1
+	db 1 ; object events
 	object_event  9, 10, SPRITE_RED, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Red, EVENT_RED_IN_MT_SILVER
-

a7420409266f7d22f165c957a211e613b0353c70
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-04 02:49:54 -0300

    Cleanup as I go

--- engine/battle/menu.asm
+++ engine/battle/menu.asm
@@ -6,27 +6,17 @@ LoadBattleMenu:
 	call InterpretBattleMenu
 	ld a, [wMenuCursorBuffer]
 	ld [wBattleMenuCursorBuffer], a
-	call ExitMenu
-	ret
-
-SafariBattleMenu:
-; untranslated
-	ld hl, MenuHeader_0x24f4e
-	call LoadMenuHeader
-	jr Function24f19
+	jp ExitMenu
 
 ContestBattleMenu:
 	ld hl, MenuHeader_0x24f89
 	call LoadMenuHeader
-
-Function24f19:
 	ld a, [wBattleMenuCursorBuffer]
 	ld [wMenuCursorBuffer], a
 	call _2DMenu
 	ld a, [wMenuCursorBuffer]
 	ld [wBattleMenuCursorBuffer], a
-	call ExitMenu
-	ret
+	jp ExitMenu
 
 BattleMenuHeader:
 	db MENU_BACKUP_TILES ; flags
@@ -46,32 +36,6 @@ Strings24f3d:
 	db "<PKMN>@"
 	db "PACK@"
 	db "RUN@"
-
-MenuHeader_0x24f4e:
-	db MENU_BACKUP_TILES ; flags
-	menu_coords 0, 12, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1
-	dw MenuData_0x24f56
-	db 1 ; default option
-
-MenuData_0x24f56:
-	db STATICMENU_CURSOR | STATICMENU_DISABLE_B ; flags
-	dn 2, 2 ; rows, columns
-	db 11 ; spacing
-	dba Strings24f5f
-	dba Function24f7c
-
-Strings24f5f:
-	db "サファりボール×　　@" ; "SAFARI BALL×  @"
-	db "エサをなげる@" ; "THROW BAIT"
-	db "いしをなげる@" ; "THROW ROCK"
-	db "にげる@" ; "RUN"
-
-Function24f7c:
-	hlcoord 17, 13
-	ld de, wSafariBallsRemaining
-	lb bc, PRINTNUM_LEADINGZEROS | 1, 2
-	call PrintNum
-	ret
 
 MenuHeader_0x24f89:
 	db MENU_BACKUP_TILES ; flags
@@ -96,5 +60,4 @@ Function24fb2:
 	hlcoord 13, 16
 	ld de, wParkBallsRemaining
 	lb bc, PRINTNUM_LEADINGZEROS | 1, 2
-	call PrintNum
-	ret
+	jp PrintNum

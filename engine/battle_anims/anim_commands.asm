4d62e311c2ce8b307379f4dc15bf009c5fd17e99
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-05 03:42:15 -0300

    Expand ANIM_OBJ limit to $200

--- engine/battle_anims/anim_commands.asm
+++ engine/battle_anims/anim_commands.asm
@@ -271,7 +271,7 @@ RunBattleAnimCommand:
 	ret
 
 .not_done_with_anim
-	cp anim_obj_command
+	cp FIRST_ANIM_COMMAND
 	jr nc, .do_anim
 
 	ld [wBattleAnimDelay], a
@@ -285,7 +285,7 @@ RunBattleAnimCommand:
 .DoCommand:
 ; Execute battle animation command in [wBattleAnimByte].
 	ld a, [wBattleAnimByte]
-	sub anim_obj_command
+	sub FIRST_ANIM_COMMAND
 
 	ld e, a
 	ld d, 0
@@ -300,54 +300,54 @@ RunBattleAnimCommand:
 
 BattleAnimCommands::
 ; entries correspond to macros/scripts/battle_anims.asm enumeration
-	dw BattleAnimCmd_Obj
-	dw BattleAnimCmd_1GFX
-	dw BattleAnimCmd_2GFX
-	dw BattleAnimCmd_3GFX
-	dw BattleAnimCmd_4GFX
-	dw BattleAnimCmd_5GFX
-	dw BattleAnimCmd_IncObj
-	dw BattleAnimCmd_SetObj
-	dw BattleAnimCmd_IncBGEffect
-	dw BattleAnimCmd_BattlerGFX_1Row
-	dw BattleAnimCmd_BattlerGFX_2Row
-	dw BattleAnimCmd_CheckPokeball
-	dw BattleAnimCmd_Transform
-	dw BattleAnimCmd_RaiseSub
-	dw BattleAnimCmd_DropSub
-	dw BattleAnimCmd_ResetObp0
-	dw BattleAnimCmd_Sound
-	dw BattleAnimCmd_Cry
-	dw GenericDummyFunction
-	dw BattleAnimCmd_OAMOn
-	dw BattleAnimCmd_OAMOff
-	dw BattleAnimCmd_ClearObjs
-	dw BattleAnimCmd_BeatUp
-	dw GenericDummyFunction
-	dw BattleAnimCmd_UpdateActorPic
-	dw BattleAnimCmd_Minimize
-	dw GenericDummyFunction
-	dw GenericDummyFunction
-	dw GenericDummyFunction
-	dw GenericDummyFunction
-	dw BattleAnimCmd_IfParamAnd
-	dw BattleAnimCmd_JumpUntil
-	dw BattleAnimCmd_BGEffect
-	dw BattleAnimCmd_BGP
-	dw BattleAnimCmd_OBP0
-	dw BattleAnimCmd_OBP1
-	dw BattleAnimCmd_KeepSprites
-	dw ClearEnemyHud
-	dw GenericDummyFunction
-	dw GenericDummyFunction
-	dw BattleAnimCmd_IfParamEqual
-	dw BattleAnimCmd_SetVar
-	dw BattleAnimCmd_IncVar
-	dw BattleAnimCmd_IfVarEqual
-	dw BattleAnimCmd_Jump
-	dw BattleAnimCmd_Loop
-	dw BattleAnimCmd_Call
-	dw BattleAnimCmd_Ret
+	dw GenericDummyFunction            ; d0
+	dw BattleAnimCmd_1GFX              ; d1
+	dw BattleAnimCmd_2GFX              ; d2
+	dw BattleAnimCmd_3GFX              ; d3
+	dw BattleAnimCmd_4GFX              ; d4
+	dw BattleAnimCmd_5GFX              ; d5
+	dw BattleAnimCmd_IncObj            ; d6
+	dw BattleAnimCmd_SetObj            ; d7
+	dw BattleAnimCmd_IncBGEffect       ; d8
+	dw BattleAnimCmd_BattlerGFX_1Row   ; d9
+	dw BattleAnimCmd_BattlerGFX_2Row   ; da
+	dw BattleAnimCmd_CheckPokeball     ; db
+	dw BattleAnimCmd_Transform         ; dc
+	dw BattleAnimCmd_RaiseSub          ; dd
+	dw BattleAnimCmd_DropSub           ; de
+	dw BattleAnimCmd_ResetObp0         ; df
+	dw BattleAnimCmd_Sound             ; e0
+	dw BattleAnimCmd_Cry               ; e1
+	dw GenericDummyFunction            ; e2
+	dw BattleAnimCmd_OAMOn             ; e3
+	dw BattleAnimCmd_OAMOff            ; e4
+	dw BattleAnimCmd_ClearObjs         ; e5
+	dw BattleAnimCmd_BeatUp            ; e6
+	dw GenericDummyFunction            ; e7
+	dw BattleAnimCmd_UpdateActorPic    ; e8
+	dw BattleAnimCmd_Minimize          ; e9
+	dw GenericDummyFunction            ; ea
+	dw GenericDummyFunction            ; eb
+	dw GenericDummyFunction            ; ec
+	dw GenericDummyFunction            ; ed
+	dw BattleAnimCmd_IfParamAnd        ; ee
+	dw BattleAnimCmd_JumpUntil         ; ef
+	dw BattleAnimCmd_BGEffect          ; f0
+	dw BattleAnimCmd_BGP               ; f1
+	dw BattleAnimCmd_OBP0              ; f2
+	dw BattleAnimCmd_OBP1              ; f3
+	dw BattleAnimCmd_KeepSprites       ; f4
+	dw ClearEnemyHud                   ; f5
+	dw BattleAnimCmd_Obj               ; f6
+	dw BattleAnimCmd_Obj               ; f7
+	dw BattleAnimCmd_IfParamEqual      ; f8
+	dw BattleAnimCmd_SetVar            ; f9
+	dw BattleAnimCmd_IncVar            ; fa
+	dw BattleAnimCmd_IfVarEqual        ; fb
+	dw BattleAnimCmd_Jump              ; fc
+	dw BattleAnimCmd_Loop              ; fd
+	dw BattleAnimCmd_Call              ; fe
+	dw BattleAnimCmd_Ret               ; ff
 
 BattleAnimCmd_Ret:
 	ld hl, wBattleAnimFlags
@@ -563,6 +563,9 @@ BattleAnimCmd_IfParamAnd:
 
 BattleAnimCmd_Obj:
 ; index, x, y, param
+	ld a, [wBattleAnimByte]
+	and 1
+	ld [wBattleObjectTempID + 1], a
 	call GetBattleAnimByte
 	ld [wBattleObjectTempID], a
 	call GetBattleAnimByte

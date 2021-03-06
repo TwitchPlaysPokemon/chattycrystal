1729c505edb902bbed6b23cc716ee8ca20a64ff3
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-06 20:26:37 -0300

    Cleanup

--- maps/SeafloorCavernEntrance.asm
+++ maps/SeafloorCavernEntrance.asm
@@ -1,5 +1,3 @@
-	object_const_def ; object_event constants
-
 SeafloorCavernEntrance_MapScripts:
 	db 0 ; scene scripts
 
@@ -11,20 +9,7 @@ SeafloorCavernBoulder:
 SeafloorCavernRock:
 	jumpstd smashrock
 
-SeafloorCavernEntrance_MapEvents:
-	db 0, 0 ; filler
-
-	db 1 ; warp events
-	warp_event  9,  1, SEAFLOOR_CAVERN_ROOM1, 1
-
-	db 1 ; coord events
-	coord_event  3, 13, SCENE_ALWAYS, .Leave
-
-	db 0 ; bg events
-
-	db 0 ; object events
-
-.Leave
+SeafloorCavernEntrance_Leave:
 	special FadeOutMusic
 	special FadeBlackQuickly
 	playsound SFX_ENTER_DOOR
@@ -34,3 +19,16 @@ SeafloorCavernEntrance_MapEvents:
 	waitsfx
 	warp REDS_HOUSE_2F, 0, 7
 	end
+
+SeafloorCavernEntrance_MapEvents:
+	db 0, 0 ; filler
+
+	db 1 ; warp events
+	warp_event  9,  1, SEAFLOOR_CAVERN_ROOM1, 1
+
+	db 1 ; coord events
+	coord_event  3, 13, SCENE_ALWAYS, SeafloorCavernEntrance_Leave
+
+	db 0 ; bg events
+
+	db 0 ; object events

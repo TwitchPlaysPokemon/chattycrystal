6b5032526297e95622644b47e2d7d74b31a6c6a0
Rangi <remy.oukaour+rangi42@gmail.com>
2021-03-07 01:52:56 -0500

    More home/low.asm labels, assertions, and comments

--- home/low.asm
+++ home/low.asm
@@ -1,7 +1,7 @@
 SECTION "Low Home", ROM0
 
-	assert @ == $0000
 NULL::
+	assert NULL == $0000
 	; reset code, just in case
 	di
 	jp Start
@@ -19,6 +19,7 @@ GenericDummyFunction::
 	ds 1
 
 FarCall::
+	assert FarCall == $0008
 	; rst $08
 	jp FarCall_hl
 
@@ -32,6 +33,7 @@ xor_a_dec_a::
 	ret
 
 Bankswitch::
+	assert Bankswitch == $0010
 	; rst $10
 	ldh [hROMBank], a
 	ld [MBC3RomBank], a
@@ -40,6 +42,7 @@ Bankswitch::
 	ds 2
 
 ChattyOn::
+	assert ChattyOn == $0018
 	; rst $18
 	push af
 	xor a
@@ -50,6 +53,7 @@ ChattyOn::
 	ds 1
 
 ChattyOff::
+	assert ChattyOff == $0020
 	; rst $20
 	push af
 	ld a, 1
@@ -58,6 +62,7 @@ ChattyOff::
 	ret
 
 JumpTable::
+	assert JumpTable == $0028
 	; rst $28
 	push de
 	ld e, a
@@ -72,31 +77,37 @@ JumpTable::
 
 	ds 5
 
-	assert @ == $0038
-; no rst, just a crash
-	rst $38
+Crash::
+	assert Crash == $0038
+	; no rst, just a crash
+	rst Crash
 
 	ds 7
 
 	assert @ == $0040
+	; vblank interrupt
 	jp VBlank
 
 	ds 5
 
 	assert @ == $0048
+	; lcd interrupt
 	jp LCD
 
 	ds 5
 
 	assert @ == $0050
+	; timer interrupt
 	jp Timer
 
 	ds 5
 
 	assert @ == $0058
+	; serial interrupt
 	jp Serial
 
 	ds 5
 
 	assert @ == $0060
+	; joypad interrupt
 	jp JoypadInt

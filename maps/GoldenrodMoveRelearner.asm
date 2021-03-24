21254520b0de1d75f5c774b9b8fd095a9d68182c
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-24 06:27:13 -0300

    Move relearner script

--- maps/GoldenrodMoveRelearner.asm
+++ maps/GoldenrodMoveRelearner.asm
@@ -7,12 +7,58 @@ GoldenrodMoveRelearner_MapScripts:
 	db 0 ; callbacks
 
 GoldenrodMoveRelearner_RelearnerScript:
-	; TODO: fix up
-	jumptextfaceplayer .test
+	faceplayer
+	opentext
+	writetext .introduction_text
+	special PlaceMoneyTopRight
+	yesorno
+	iffalse .cancel
+	checkmoney YOUR_MONEY, 2500
+	ifequal HAVE_LESS, .not_enough_money
+	waitsfx
+	playsound SFX_TRANSACTION
+	takemoney YOUR_MONEY, 2500
+	special PlaceMoneyTopRight
+	farscall MoveRelearnerScript
+	iftrue .end
+	givemoney YOUR_MONEY, 2500
+.end
+	end
 
-.test
-	text "Testing: OK!"
+.introduction_text
+	text "Welcome to my"
+	line "house. I can make"
+	para "#MON remember"
+	line "the moves that"
+	cont "they used to know."
+
+	para "For ¥2500, I can"
+	line "make one of your"
+	para "#MON remember a"
+	line "forgotten move."
+
+	para "What do you say?"
+	line "It's only ¥2500."
 	done
+
+.not_enough_money
+	writetext .not_enough_money_text
+	waitbutton
+	closetext
+	end
+
+.not_enough_money_text
+	text "I'm sorry, but it"
+	line "seems like you"
+	para "can't afford that"
+	line "right now."
+
+	para "Please come again"
+	line "when you can."
+	done
+
+.cancel
+	farsjump MoveRelearnerCancel
 
 GoldenrodMoveRelearnerBookshelf1:
 	jumpstd magazinebookshelf

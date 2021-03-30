621288d339292f4de53b3633bab9d3cac78f5f4d
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-03-30 08:01:47 -0300

    Remove tabs within text

--- audio/cries.asm
+++ audio/cries.asm
@@ -1018,9 +1018,9 @@ Cry_Weepinbell:
 	musicheader 1, 8, Cry_Weepinbell_Ch8
 
 Cry_Unused:
-	musicheader	3, 5, Unused_Cry_Ch5
-	musicheader	1, 6, Unused_Cry_Ch6
-	musicheader	1, 8, Unused_Cry_Ch8
+	musicheader 3, 5, Unused_Cry_Ch5
+	musicheader 1, 6, Unused_Cry_Ch6
+	musicheader 1, 8, Unused_Cry_Ch8
 
 Unused_Cry_Ch5:
 	sound_duty 0, 0, 3, 3

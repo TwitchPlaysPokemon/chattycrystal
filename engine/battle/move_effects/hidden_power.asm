d5913e33e976967289de44f9438758a946fd3213
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-04-02 16:51:38 -0300

    Add non-button-based enemy/link mode chatty HP

--- engine/battle/move_effects/hidden_power.asm
+++ engine/battle/move_effects/hidden_power.asm
@@ -27,8 +27,23 @@ BattleCommand_NonChattyHP:
 	ld a, [wAttackMissed]
 	and a
 	ret nz
+	ldh a, [hBattleTurn]
+	and a
+	ld hl, wEnemyMonLevel
+	jr nz, .no_buttons
 	ld a, [wLinkMode]
 	and a
-	jr nz, BattleCommand_HiddenPower
+	ld hl, wBattleMonLevel
+	jr nz, .no_buttons
 	farcall ChattyHiddenPowerDamage
+	ret
+
+.no_buttons
+	; link mode or enemy mon - just set power to level + 20; the attack is ??? type (x2 on everything)
+	push hl
+	farcall BattleCommand_DamageStats
+	pop hl
+	ld a, [hl]
+	add a, 20
+	ld d, a
 	ret

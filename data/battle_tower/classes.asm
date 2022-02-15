42dd2490eec9eef8f16a2ff3f686aed96b4b9360
SabataLunar <Lordhelix777@yahoo.co.jp>
2022-02-15 17:44:50 +0000

    Refill list
    This list does not like being shrunk

--- data/battle_tower/classes.asm
+++ data/battle_tower/classes.asm
@@ -1,21 +1,75 @@
 BattleTowerTrainers:
 ; The trainer class is not used in Crystal 1.0 due to a bug.
 ; Instead, the sixth character in the trainer's name is used.
-; See BattleTowerText in engine/events/battle_tower/trainer_text.asm.
+; See BattleTowerText in engine/events/battle_tower/trainer_text.asm. do not delete trainers
 	db "Kr@@@@@@@@", PT_LUCAS
 	db "ACCPPQ@@@@", GREEN_MAY
 	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+; The following can only be sampled in Crystal 1.1.
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
 	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "Kr@@@@@@@@", PT_LUCAS
+	db "ACCPPQ@@@@", GREEN_MAY
+	db "ABBBCC@@@@", XD_MICHAEL
+	db "WONG@@@@@@", FIREBREATHER

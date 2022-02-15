1017ee5a47cf909d12a0289afc5de8834e14d688
SabataLunar <Lordhelix777@yahoo.co.jp>
2022-02-15 18:37:13 +0000

    Removed Tower Dialogue
    Hosts should remain silent now

--- data/battle_tower/trainer_text.asm
+++ data/battle_tower/trainer_text.asm
@@ -1,623 +1,479 @@
 BattleTowerText_0x1ec000:
-	text "Hello, glad to"
-	line "meet you!"
-
-	para "I do hope we have"
-	line "a good battle."
+	text "<……>"
 	done
 
 BattleTowerText_0x1ec03b:
-	text "Thank you! A most"
-	line "enjoyable battle!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec060:
-	text "Thank you. You are"
-	line "formidable."
+	text "<……>"
 	done
 
 BattleTowerText_0x1ec080:
-	text "Work, work, work…"
-	line "I'm always busy!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec0a3:
-	text "But, I work hard"
-	line "in battle too!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec0c4:
-	text "I'm too busy to be"
-	line "battling!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec0e1:
-	text "Brace yourself for"
-	line "my all-out attack!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec108:
-	text "What a cakewalk!"
-	line "You're too easy!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec12a:
-	text "I won't lose next"
-	line "time, all right?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec14d:
-	text "Heh, your #MON"
-	line "look pretty cool."
+	text "<……>"
 	done
 
 UnknownText_0x1ec16f:
-	text "Hey, hey, no way!"
-	line "You won't win!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec190:
-	text "You serious?"
-	line "This is brutal!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec1ae:
-	text "#MON every day!"
-	line "I love battling!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec1d0:
-	text "I'm on top of my"
-	line "game, but not you!"
+	text "<……>"
 	done
-
+	
 UnknownText_0x1ec1f4:
-	text "I don't care that"
-	line "I lost, really!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec216:
-	text "Hi, there! Let's"
-	line "keep this clean!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec238:
-	text "Whoops, sorry for"
-	line "that wipeout!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec259:
-	text "Whoops! Come on,"
-	line "let me win one!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec27b:
-	text "Do you want to see"
-	line "my battle level?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec2a0:
-	text "Hehehe, I know"
-	line "your level now!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec2c0:
-	text "Ouch… I'm just too"
-	line "weak…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec2d9:
-	text "Hey, let's battle."
-	line "I'm your opponent."
+	text "<……>"
 	done
 
 UnknownText_0x1ec2fe:
-	text "Wow, you're not"
-	line "serious about it!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec320:
-	text "…Urgh… Nothing"
-	line "positive here…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec33f:
-	text "I'm your"
-	line "opponent."
-
-	para "Get ready to be"
-	line "hammered."
+	text "<……>"
 	done
 
 UnknownText_0x1ec36c:
-	text "Hahah! That was a"
-	line "pushover!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec389:
-	text "No way! There has"
-	line "to be a mistake!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec3ad:
-	text "Hah!"
-	line "Let's get rolling!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec3c5:
-	text "Wahahaha! Didn't"
-	line "break a sweat!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec3e5:
-	text "Tough! I'm no"
-	line "match for you!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec402:
-	text "<……><……><……>"
-	line "<……><……>Battle?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec411:
-	text "<……><……><……>"
-	line "<……><……>I won?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec41f:
-	text "<……><……><……>"
-	line "<……><……>I lost?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec42e:
-	text "You want to be a"
-	line "leader?"
-
-	para "Let me battle you,"
-	line "then!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec461:
-	text "You need another"
-	line "ten years of"
-
-	para "training to get"
-	line "better, I'd say."
+	text "<……>"
 	done
 
 UnknownText_0x1ec4a0:
-	text "You're incredibly"
-	line "talented."
-
-	para "No one can touch"
-	line "you now!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec4d6:
-	text "Today, I'm going"
-	line "to whomp you."
+	text "<……>"
 	done
 
 UnknownText_0x1ec4f5:
-	text "I knew I'd win."
-	line "I'm so great!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec512:
-	text "Uh? My plans are"
-	line "out of whack…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec532:
-	text "I bet you can't"
-	line "beat me!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec54b:
-	text "Those #MON"
-	line "aren't enough!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec565:
-	text "I want your"
-	line "#MON. Please?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec580:
-	text "I'll show you a"
-	line "real battle!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec59d:
-	text "This battle…"
-	line "I'm bored!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec5b5:
-	text "…I won't turn tail"
-	line "in battle!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec5d3:
-	text "Let's go!"
-	line "No holds barred!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec5ee:
-	text "Sorry! I wanted"
-	line "the win more!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec60d:
-	text "Wahah! Congrats!"
-	line "I can't do better!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec631:
-	text "My #MON skills"
-	line "are phenomenal!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec651:
-	text "You've got a long"
-	line "way to go."
-
-	para "Well, keep trying!"
-	line "Best of luck!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec68f:
-	text "Aww… Don't lose"
-	line "after beating me."
+	text "<……>"
 	done
 
 UnknownText_0x1ec6b1:
-	text "Who are you?"
-	line "I don't know you…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec6d0:
-	text "I must've imagined"
-	line "that."
-
-	para "There seems to be"
-	line "no one here…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec708:
-	text "Who am I?"
-	line "I don't know…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec720:
-	text "Um… Are you that…"
-	line "um…person?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec73e:
-	text "It doesn't appear"
-	line "to be you…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec75b:
-	text "Then you really"
-	line "are the legendary…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec77f:
-	text "I heard that"
-	line "you're hot!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec798:
-	text "Not bad. I was"
-	line "just a bit better."
+	text "<……>"
 	done
 
 UnknownText_0x1ec7bb:
-	text "Eh, you're not"
-	line "that special."
+	text "<……>"
 	done
 
 UnknownText_0x1ec7d8:
-	text "I'm scared about"
-	line "what might happen."
-
-	para "My #MON are way"
-	line "too strong."
+	text "<……>"
 	done
 
 UnknownText_0x1ec818:
-	text "See? My #MON"
-	line "were too strong."
+	text "<……>"
 	done
 
 UnknownText_0x1ec837:
-	text "Graa! My #MON"
-	line "were total wimps!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec858:
-	text "Hey, there!"
-	line "I'll take you on!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec876:
-	text "Don't you have a"
-	line "better strategy?"
+	text "<……>"
 	done
 
 UnknownText_0x1ec898:
-	text "You've got decent"
-	line "style!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec8b1:
-	text "I wonder if I can"
-	line "battle properly…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec8d5:
-	text "Um… Sorry…"
-	line "I think I won."
+	text "<……>"
 	done
 
 UnknownText_0x1ec8f0:
-	text "I guess I'm not"
-	line "good enough yet…"
+	text "<……>"
 	done
 
 UnknownText_0x1ec911:
-	text "Wrrooar!"
-	line "I won't lose!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec928:
-	text "Wrrooar! I knew"
-	line "I was a genius!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec949:
-	text "Arrooh! I hate it"
-	line "when I lose!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec969:
-	text "Sorry, but I'm"
-	line "going to win."
+	text "<……>"
 	done
 
 UnknownText_0x1ec986:
-	text "Yeah! My #MON"
-	line "rule!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec99b:
-	text "Oh, close! I lost"
-	line "by just a bit!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec9bd:
-	text "OK, I'm not"
-	line "fooling around!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec9d9:
-	text "Yay! Too easy!"
-	line "Like, no way!"
+	text "<……>"
 	done
 
 UnknownText_0x1ec9f7:
-	text "No!"
-	line "Like, no way!"
+	text "<……>"
 	done
 
 UnknownText_0x1eca0a:
-	text "Look! My #MON"
-	line "are really cute!"
+	text "<……>"
 	done
 
 UnknownText_0x1eca2a:
-	text "Aren't they really"
-	line "adorable?"
+	text "<……>"
 	done
 
 UnknownText_0x1eca47:
-	text "I'm sorry, it's"
-	line "all my fault!"
+	text "<……>"
 	done
 
 UnknownText_0x1eca64:
-	text "Let's get our"
-	line "battle started!"
+	text "<……>"
 	done
 
 UnknownText_0x1eca82:
-	text "Was I too strong"
-	line "for you?"
+	text "<……>"
 	done
 
 UnknownText_0x1eca9d:
-	text "Ooh, you're in a"
-	line "different class."
+	text "<……>"
 	done
 
 UnknownText_0x1ecabf:
-	text "Are we going to"
-	line "battle? Let's!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecade:
-	text "Oh, you're too"
-	line "weak. Shame."
+	text "<……>"
 	done
 
 UnknownText_0x1ecafa:
-	text "Wow! Are you quite"
-	line "satisfied?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecb19:
-	text "Oh, you have some"
-	line "rare #MON."
+	text "<……>"
 	done
 
 UnknownText_0x1ecb37:
-	text "May I have one of"
-	line "your #MON?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecb55:
-	text "…I want one of"
-	line "your #MON."
+	text "<……>"
 	done
 
 UnknownText_0x1ecb70:
-	text "Want to hear about"
-	line "my cute #MON?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecb92:
-	text "What do you think"
-	line "about my cuties?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecbb6:
-	text "Oh! My! You're a"
-	line "dreadful trainer!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecbd9:
-	text "Battle? Sure!"
-	line "Right now!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecbf3:
-	text "Oh, I love it!"
-	line "Battling is wild!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecc15:
-	text "Oh, how rude! Wait"
-	line "till next time!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecc39:
-	text "Please let me win!"
-	line "Please?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecc55:
-	text "Wow, thank you!"
-	line "You're so nice!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecc75:
-	text "You're mean!"
-	line "I hate meanies!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecc92:
-	text "Well, can we"
-	line "begin?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecca7:
-	text "Well, I beg your"
-	line "pardon…"
+	text "<……>"
 	done
 
 UnknownText_0x1eccc1:
-	text "Sob… That's not"
-	line "fair!"
+	text "<……>"
 	done
 
 UnknownText_0x1eccd7:
-	text "I'm good!"
-	line "You can't win."
+	text "<……>"
 	done
 
 UnknownText_0x1eccef:
-	text "Giving up? You're"
-	line "pretty weak!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecd0e:
-	text "I won't accept"
-	line "this… No way!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecd2b:
-	text "Are you treating"
-	line "this seriously?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecd4d:
-	text "Oh, sorry! Looks"
-	line "like I won!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecd6b:
-	text "Oh, how nasty!"
-	line "You were serious!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecd8d:
-	text "Ahahah! I'll take"
-	line "it easy on you!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecdaf:
-	text "Oops, sorry! But"
-	line "I'm happy too!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecdcf:
-	text "Oh, oh, I lost!"
-	line "Thanks. Bye!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecded:
-	text "BATTLE TOWER is a"
-	line "tough place!"
+	text "<……>"
 	done
 
 UnknownText_0x1ece0d:
-	text "You might have a"
-	line "hard time."
+	text "<……>"
 	done
 
 UnknownText_0x1ece2a:
-	text "Ooh, you might"
-	line "make a run here!"
+	text "<……>"
 	done
 
 UnknownText_0x1ece4b:
-	text "I want to see your"
-	line "style in action!"
+	text "<……>"
 	done
 
 UnknownText_0x1ece70:
-	text "Every battle is a"
-	line "drama!"
+	text "<……>"
 	done
 
 UnknownText_0x1ece8a:
-	text "Oh… Want to trade"
-	line "something?"
+	text "<……>"
 	done
 
 UnknownText_0x1ecea8:
-	text "OK, here goes!"
-	line "I have momentum!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecec9:
-	text "See, I rolled"
-	line "right over you!"
+	text "<……>"
 	done
 
 UnknownText_0x1ecee8:
-	text "No! This did not"
-	line "happen!"
+	text "<……>"
 	done

b464e078bcb84735397d95eae7eaecc5c7c98fc4
SabataLunar <Lordhelix777@yahoo.co.jp>
2022-02-15 19:11:35 +0000

    readd silence

--- data/battle_tower/trainer_text.asm
+++ data/battle_tower/trainer_text.asm
@@ -1,479 +1,479 @@
 BattleTowerText_0x1ec000:
-	text "test"
+	text "………"
 	done
 
 BattleTowerText_0x1ec03b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec060:
-	text "test"
+	text "………"
 	done
 
 BattleTowerText_0x1ec080:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec0a3:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec0c4:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec0e1:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec108:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec12a:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec14d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec16f:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec190:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec1ae:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec1d0:
-	text "test"
+	text "………"
 	done
 	
 UnknownText_0x1ec1f4:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec216:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec238:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec259:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec27b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec2a0:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec2c0:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec2d9:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec2fe:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec320:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec33f:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec36c:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec389:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec3ad:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec3c5:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec3e5:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec402:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec411:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec41f:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec42e:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec461:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec4a0:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec4d6:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec4f5:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec512:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec532:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec54b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec565:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec580:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec59d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec5b5:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec5d3:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec5ee:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec60d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec631:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec651:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec68f:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec6b1:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec6d0:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec708:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec720:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec73e:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec75b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec77f:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec798:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec7bb:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec7d8:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec818:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec837:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec858:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec876:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec898:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec8b1:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec8d5:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec8f0:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec911:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec928:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec949:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec969:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec986:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec99b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec9bd:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec9d9:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ec9f7:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca0a:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca2a:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca47:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca64:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca82:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eca9d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecabf:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecade:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecafa:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecb19:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecb37:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecb55:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecb70:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecb92:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecbb6:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecbd9:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecbf3:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecc15:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecc39:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecc55:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecc75:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecc92:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecca7:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eccc1:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eccd7:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1eccef:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecd0e:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecd2b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecd4d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecd6b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecd8d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecdaf:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecdcf:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecded:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ece0d:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ece2a:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ece4b:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ece70:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ece8a:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecea8:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecec9:
-	text "test"
+	text "………"
 	done
 
 UnknownText_0x1ecee8:
-	text "test"
+	text "………"
 	done
